0095915aab5915d61821ae45a5cc2d85e9e0a7b7
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-11-26 15:02:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("./header.php") ?>
+
+    <h1>Hello word</h1>
+    <button type="button" class="btn btn-outline-primary">Generar boletas de 4 oportunidades</button>
+
+    <button type="button" class="btn btn-outline-primary">Generar boletas de 8 oportunidades</button>
+
+<?php include("./footer.php") ?>
