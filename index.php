d807e34a46c1f020da2a391ad6b4760a103fc321
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-11-26 15:09:37 -0300

    First commit

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include("./header.php") ?>
-
-    <h1>Hello word</h1>
-    <button type="button" class="btn btn-outline-primary">Generar boletas de 4 oportunidades</button>
-
-    <button type="button" class="btn btn-outline-primary">Generar boletas de 8 oportunidades</button>
-
-<?php include("./footer.php") ?>
---
+++ pages/index.php
@@ -0,0 +1,12 @@
+<?php include("../includes/header.php") ?>
+
+    <div class="text-center">
+        <h1>Bienvenido a Susuerte</h1>
+    </div>
+    <div class="text-center">
+        <button type="button" class="btn btn-outline-primary">Generar boletas de 4 oportunidades</button>
+        
+        <button type="button" class="btn btn-outline-primary">Generar boletas de 8 oportunidades</button>
+    </div>
+
+<?php include("../includes/footer.php") ?>
