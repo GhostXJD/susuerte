1613032c41e51854521fa4b147193dee17d0cc4a
GxStyle <82037365+GxStyle@users.noreply.github.com>
2023-11-29 21:51:15 -0300

    Datos sin repetir

--- functions/generate4.php
+++ functions/generate4.php
@@ -1,35 +1,57 @@
 <?php
 session_start();
+
 function generarLista10000() {
-    $numeros = array();
-    for ($i = 0; $i < 10000; $i++) {
-        $numeros[] = rand(1000, 9999);
-    }
-    return $numeros;
+    $numeros = range(0, 9999);
+    shuffle($numeros);
+    $numeros_con_ceros = array_map(function ($numero) {
+        return str_pad($numero, 4, '0', STR_PAD_LEFT);
+    }, $numeros);
+    return $numeros_con_ceros;
 }
 
-function cuatroNumero($lista) {
-    $boletas = array();
-    for ($i = 0; $i < count($lista); $i += 4) {
-        $boleta = array_slice($lista, $i, 4);
-        $boleta_sin_repetidos = array_unique($boleta);
-        while (count($boleta) !== count($boleta_sin_repetidos)) {
-            $boleta = array();
-            for ($j = 0; $j < 4; $j++) {
-                $boleta[] = rand(1000, 9999);
-            }
-            $boleta_sin_repetidos = array_unique($boleta);
-        }
-        $boletas[] = $boleta;
-    }
-    return array_slice($boletas, 0, 2500);
+function dividirEnGrupos($lista) {
+    $grupos = array_chunk($lista, 4);
+    return $grupos;
 }
+
+function eliminarRepetidos($grupos) {
+    $boletas = array();
+    $numeros_utilizados = array();
+
+    foreach ($grupos as $grupo) {
+        $nuevo_grupo = array();
+        foreach ($grupo as $numero) {
+            if (!in_array($numero, $numeros_utilizados)) {
+                $nuevo_grupo[] = $numero;
+                $numeros_utilizados[] = $numero;
+            }
+        }
+
+        // Si el grupo actual no tiene suficientes números únicos, se completará con números restantes sin repetir
+        while (count($nuevo_grupo) < 4) {
+            $numero_faltante = array_diff(range(0, 9999), $numeros_utilizados);
+            $numero_seleccionado = array_shift($numero_faltante);
+            $nuevo_grupo[] = $numero_seleccionado;
+            $numeros_utilizados[] = $numero_seleccionado;
+        }
+
+        $boletas[] = $nuevo_grupo;
+    }
+
+    return $boletas;
+}
+
+$lista = generarLista10000();
+$grupos = dividirEnGrupos($lista);
+$boletas_sin_repetidos = eliminarRepetidos($grupos);
+
+$boletas_finales = array_slice($boletas_sin_repetidos, 0, 2500);
+
 
 if (isset($_POST['generate_4'])) {
-    $numeros_aleatorios = generarLista10000();
-    $boletas_cuatro_numeros = cuatroNumero($numeros_aleatorios);
 
-    $_SESSION['boletas_generadas_4'] = $boletas_cuatro_numeros;
+    $_SESSION['boletas_generadas_4'] = $boletas_finales;
 
     header("Location: ../pdf4.php");
     exit();
--- functions/generate8.php
+++ functions/generate8.php
@@ -1,37 +1,58 @@
 <?php
 session_start();
+
 function generarLista10000() {
-    $numeros = array();
-    for ($i = 0; $i < 10000; $i++) {
-        $numeros[] = rand(1000, 9999);
-    }
-    return $numeros;
+    $numeros = range(0, 9999);
+    shuffle($numeros);
+    $numeros_con_ceros = array_map(function ($numero) {
+        return str_pad($numero, 4, '0', STR_PAD_LEFT);
+    }, $numeros);
+    return $numeros_con_ceros;
 }
 
-function ochoNumero($lista) {
-    $boletas = array();
-    for ($i = 0; $i < count($lista); $i += 8) {
-        $boleta = array_slice($lista, $i, 8);
-        $boleta_sin_repetidos = array_unique($boleta);
-        while (count($boleta) !== count($boleta_sin_repetidos)) {
-            $boleta = array();
-            for ($j = 0; $j < 8; $j++) {
-                $boleta[] = rand(1000, 9999);
-            }
-            $boleta_sin_repetidos = array_unique($boleta);
-        }
-        $boletas[] = $boleta;
-    }
-    return array_slice($boletas, 0, 1250);
+function dividirEnGrupos($lista) {
+    $grupos = array_chunk($lista, 8);
+    return $grupos;
 }
+
+function eliminarRepetidos($grupos) {
+    $boletas = array();
+    $numeros_utilizados = array();
+
+    foreach ($grupos as $grupo) {
+        $nuevo_grupo = array();
+        foreach ($grupo as $numero) {
+            if (!in_array($numero, $numeros_utilizados)) {
+                $nuevo_grupo[] = $numero;
+                $numeros_utilizados[] = $numero;
+            }
+        }
+        while (count($nuevo_grupo) < 4) {
+            $numero_faltante = array_diff(range(0, 9999), $numeros_utilizados);
+            $numero_seleccionado = array_shift($numero_faltante);
+            $nuevo_grupo[] = $numero_seleccionado;
+            $numeros_utilizados[] = $numero_seleccionado;
+        }
+
+        $boletas[] = $nuevo_grupo;
+    }
+
+    return $boletas;
+}
+
+$lista = generarLista10000();
+$grupos = dividirEnGrupos($lista);
+$boletas_sin_repetidos = eliminarRepetidos($grupos);
+
+$boletas_finales = array_slice($boletas_sin_repetidos, 0, 2500);
+
 
 if (isset($_POST['generate_8'])) {
-    $numeros_aleatorios = generarLista10000();
-    $boletas_ocho_numeros = ochoNumero($numeros_aleatorios);
 
-    $_SESSION['boletas_generadas_8'] = $boletas_ocho_numeros;
+    $_SESSION['boletas_generadas_8'] = $boletas_finales;
 
     header("Location: ../pdf8.php");
     exit();
 }
+
 ?>
