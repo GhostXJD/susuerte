2d673932603024c8136c462e0312db798ff444d9
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-12-01 01:14:14 -0300

    Fondo a las paginas

--- pdf8.php
+++ pdf8.php
@@ -2,11 +2,34 @@
 
 require_once('./TCPDF-main/tcpdf.php');
 
+/// Extend the TCPDF class to create custom Header and Footer
+class MYPDF extends TCPDF {
+    //Page header
+    public function Header() {
+        // get the current page break margin
+        $bMargin = $this->getBreakMargin();
+        // get current auto-page-break mode
+        $auto_page_break = $this->AutoPageBreak;
+        // disable auto-page-break
+        $this->SetAutoPageBreak(false, 0);
+        // set bacground image
+        $img_file = K_PATH_IMAGES.'plata.jpg';
+        $this->Image($img_file, 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
+        // restore auto-page-break status
+        $this->SetAutoPageBreak($auto_page_break, $bMargin);
+        // set the starting point for the page content
+        $this->setPageMark();
+    }
+}
+
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set auto page breaks
 $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 // ---------------------------------------------------------
 
@@ -18,7 +41,6 @@ $pdf->AddPage();
 
 $pdf->SetFont('helvetica', '', 8);
 
-// -----------------------------------------------------------------------------
 $pdf->setEqualColumns(2, 105);
 
 session_start();
@@ -32,7 +54,7 @@ if (isset($_SESSION['boletas_generadas_8'])) {
     <style>
 
     td {
-		height: 24px;
+		height: 30px;
         font-weight: bold;
 	}
     th {
